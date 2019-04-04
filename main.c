a8aa1e1ddfd7484409ae58b312c429cf178056f9
Ryan Mistelbauer <c3305432@uon.edu.au>
2019-04-04 04:15:03 +0000

    Added Pseudocode and a working encryption without shifting remaining letters from end back to the start

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main{
-
-return 0;
+int main(){
+    
+    /* Pseudocode
+     * Encrytion/Decryption:
+     * For encryption: 
+    1. Take a character from a string array
+    2. Take such charcter and knowing the key, add the character together with the key.
+    3. Assign new values to the array
+        IF ( character + key > 26)
+            new_character = (25 + key) - character
+    */
+   
+   char array[]= {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+   char array2[26];
+   int key = 2;
+   int index;
+   int k;
+   
+    
+    
+    
+    for (index = 0; index <= 26; index++){
+        k = index;
+        array2[k] = array[index] + key;
+        
+        if ((array[index]+ key) > 90 ){
+            array2[k] = (115) - array[index];
+        }
+        printf("%c", array[index]);
+        printf("%c \n", array2[k]);
+    }
+    return 0;
 }
