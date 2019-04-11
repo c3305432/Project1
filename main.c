d446add4bec88d0ad6588211cfdff6e13e26a4cd
Ryan Mistelbauer <c3305432@uon.edu.au>
2019-04-11 05:37:06 +0000

    Added Rotation Decryption

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <math.h>
 
 char rotationEncrypt();
+char rotationDecrypt();
 
 int main(){
     int choice;
     
+    /* Print information to console for user to read 
+       and then enter an integer to signal their choice */  
     printf("(1) Rotation Encryption\n");
     printf("(2) Rotation Decryption\n");
     printf("(3) Substitution Encryption\n");
@@ -13,14 +16,14 @@ int main(){
     printf("(5) Rotation Decryption without a key\n");
     printf("(6) Substitution Decryption without a key\n");
     printf("Please enter your choice number> ");
-    scanf("%d", &choice);
+    scanf("%d", &choice); //Scan in choice and assign it to the variable choice
     
-    switch  (choice){
+    switch  (choice){ //
         case 1:
             rotationEncrypt();
             break;
         case 2:
-            printf("Currently Under Development\n");
+            rotationDecrypt();
             break;
         case 3:
             printf("Currently Under Development\n");
@@ -44,29 +47,56 @@ int main(){
 }
 
 char rotationEncrypt(){
-    char array[1024] = {"LET'S PARTY!\n"};
+    char array[1024] = {"RYAN MISTELBAUER\n"};
     char array2[1024];
     int key = 4;
     int index;
     int k;
-   
     
-    
-    
-    for (index = 0; index < 26 - key; index++){
+
+    for (index = 0; index < 1024; index++){
         if (array[index] < 65 || array[index] > 90){
             k = index;
             array2[k] = array[index];
             printf("%c", array2[k]);
-            } else if (array[index]+ key > 90){
+            } else if (array[index] + key > 90){
                k = index;
-               array2[k] = 155 - array[index];
+               array2[k] = 156 - array[index];
                printf("%c", array2[k]); 
             } else {
             k = index;
             array2[k] = array[index] + key;
             printf("%c", array2[k]);
         }
+         
+    }
+    return array2[k];
+}
+
+char rotationDecrypt(){
+    
+    char array[1024] = {"GIWEV XLI TPIEWIV!!!\n"};
+    char array2[1024];
+    int key = 4;
+    int index;
+    int k;
+    
+
+    for (index = 0; index < 1024; index++){
+        if (array[index] < 65 || array[index] > 90){
+            k = index;
+            array2[k] = array[index];
+            printf("%c", array2[k]);
+            } else if (array[index] - key < 65){
+               k = index;
+               array2[k] = array[index] + (26 - key);
+               printf("%c", array2[k]); 
+            } else {
+            k = index;
+            array2[k] = array[index] - key;
+            printf("%c", array2[k]);
+        }
+         
     }
     return array2[k];
 }
